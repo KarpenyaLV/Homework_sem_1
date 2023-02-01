a821bfe37f317149db6b671aa91ee80668aa94c3
Lyudmila Karpenya <lv.karpenya@mail.ru>
2023-02-01 15:52:32 +0300

    Задача 4

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+//     if 
+//     (a > b)
+//     Console.WriteLine(a);
+//     else
+//     Console.WriteLine(b);
+
+
+// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+    if 
+    (a > b && a > c)
+    Console.WriteLine(a);
+    else if 
+    (b > c)
+    Console.WriteLine(b);
+    else
+    Console.WriteLine(c);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
